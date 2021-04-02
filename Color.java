6b16c0c8c3500f754bd68752fa9fcd7102a7ce53
Dredd14 <65961936+LeoChaillard@users.noreply.github.com>
2021-04-02 10:28:51 +0200

    Ajout classe color et base pour classe Player

---
+++ Color.java
@@ -0,0 +1,16 @@
+/************************************************************************
+ * LP2A Project - Spring semester 2020 - Creation of a Ludo Board game
+ * Authors : Eléanore RENAUD - eleanore.renaud@utbm.fr and Léo CHAILLARD - leo.chaillard@utbm.fr
+ * Creation date : April, 2021
+ ************************************************************************/
+
+import java.lang.*;
+import java.util.*;
+
+public enum Color
+{
+  BLEU,
+  ROUGE,
+  VERT,
+  JAUNE
+}
--- Player.java
+++ Player.java
@@ -1,8 +1,45 @@
+/************************************************************************
+ * LP2A Project - Spring semester 2021 - Creation of a Ludo Board game
+ * Authors : Eléanore RENAUD - eleanore.renaud@utbm.fr and Léo CHAILLARD - leo.chaillard@utbm.fr
+ * Creation date : April, 2021
+ ************************************************************************/
+
 import java.lang.*;
 import java.util.*;
 
 public class Player {
+  //Attributes
+  private String name;
+  private Color color;
+  private Random rand;
+  private int pawnsHome;
+
+  //Constructor
+  public Player(String n,Color c)
+  {
+    this.pawnsHome = 0;
+    this.name = n;
+    this.color = c;
+    rand = new Random();
+  }
+
+  //Methods
+  public int rollDice()
+  {
+    int n = 0;
+    while( n <= 0) n = this.rand.nextInt(7);
+    return n;
+  }
+
+  /***************************************************/
+
+  //Tests
+  public static void main(String [] args)
+  {
+    Player p = new Player("jean",Color.BLEU);
+    p.rollDice();
 
 
+	}
 
 }
