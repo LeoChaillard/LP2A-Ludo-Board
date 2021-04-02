65727f6b3da906d996f3b79eab0d67d060226928
Dredd14 <65961936+LeoChaillard@users.noreply.github.com>
2021-04-02 11:26:58 +0200

    Updates

--- GameBoard.java
+++ GameBoard.java
@@ -1,8 +1,22 @@
+/************************************************************************
+ * LP2A Project - Spring semester 2021 - Creation of a Ludo Board game
+ * Authors : Eléanore RENAUD - eleanore.renaud@utbm.fr and Léo CHAILLARD - leo.chaillard@utbm.fr
+ * Creation date : April, 2021
+ ************************************************************************/
+
 import java.lang.*;
 import java.util.*;
 
 public class GameBoard {
+  //Attributes
+  private List<Player> players = new ArrayList<Player>(4);
 
+  //Constructor
+
+
+
+
+  //Methods
 
 
 }
--- Main.java
+++ Main.java
@@ -1,3 +1,9 @@
+/************************************************************************
+ * LP2A Project - Spring semester 2021 - Creation of a Ludo Board game
+ * Authors : Eléanore RENAUD - eleanore.renaud@utbm.fr and Léo CHAILLARD - leo.chaillard@utbm.fr
+ * Creation date : April, 2021
+ ************************************************************************/
+
 import java.lang.*;
 import java.util.*;
 
@@ -5,7 +11,7 @@ public class Main {
 
   public static void main(String [] args)
   {
-    
+    GameBoard game = new GameBoard();
 
 
 
