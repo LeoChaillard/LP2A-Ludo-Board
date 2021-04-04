db5296a605d7092c3ab19d6766839d5b523ff641
Eleanore <eleanore.renaud@gmail.com>
2021-04-04 15:10:38 +0200

    Update of the files for Léo

--- Pawn.java
+++ Pawn.java
@@ -20,11 +20,43 @@ public class Pawn {
   }
 
   //Methods
-  public void movePawn(Vector newPos)
+  public void movePawn(Vector Array[], int resultDice)
   {
-    position = newPos;
+	 int currentPosition = -1;
+	 for(int i = 0; i<57; i++) {
+		 if(position.getx() == Array[i].getx() && position.gety() == Array[i].gety()) {
+			 currentPosition = i;
+			 break;
+		 }
+	 }
+	 if (resultDice == 6 && currentPosition < 51) {
+		 this.position.setx(Array[currentPosition + 6].getx());
+		 this.position.sety(Array[currentPosition + 6].gety());
+	 }
+	 else if (resultDice == 5 && currentPosition < 52) {
+		 this.position.setx(Array[currentPosition + 5].getx());
+		 this.position.sety(Array[currentPosition + 5].gety());
+	 }
+	 else if (resultDice == 4 && currentPosition < 53) {
+		 this.position.setx(Array[currentPosition + 4].getx());
+		 this.position.sety(Array[currentPosition + 4].gety());
+	 }
+	 else if (resultDice == 3 && currentPosition < 54) {
+		 this.position.setx(Array[currentPosition + 3].getx());
+		 this.position.sety(Array[currentPosition + 3].gety());
+	 }
+	 else if (resultDice == 2 && currentPosition < 55) {
+		 this.position.setx(Array[currentPosition + 2].getx());
+		 this.position.sety(Array[currentPosition + 2].gety());
+	 }
+	 else if (resultDice == 1 && currentPosition < 56) {
+		 this.position.setx(Array[currentPosition + 1].getx());
+		 this.position.sety(Array[currentPosition + 1].gety());
+	 }
+    //position = newPos;
     updateSafeZone();
   }
+
 
   /***************************************************/
 
